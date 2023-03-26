396f6e10009d15930e37fbbf8c28311cfd07b495
Akbarali <Akbarali9705@mail.ru>
2023-03-26 11:29:13 +0500

    add midelware

--- public/index.php
+++ public/index.php
@@ -5,13 +5,44 @@
 //ini_set('log_errors', true);             // Error/Exception file logging engine.
 //ini_set('error_log', __DIR__.'/../public/logs/php_errors.log'); // Logging file path
 use App\App;
+use App\Middleware\RoleCheckMiddleware;
+use App\Middleware\Server;
+use App\Middleware\ThrottlingMiddleware;
+use App\Middleware\UserExistsMiddleware;
 
 session_start();
 require __DIR__.'/../vendor/autoload.php';
 require __DIR__.'/../public/bootstrap.php';
 
 try {
-    (new App())->run();
+
+    /**
+     * The client code.
+     */
+    $server = new Server();
+    $server->register("admin@example.com", "admin_pass");
+    $server->register("user@example.com", "user_pass");
+
+    // All middleware are chained. The client can build various configurations of
+    // chains depending on its needs.
+    $middleware = new ThrottlingMiddleware(2);
+    $middleware
+        ->linkWith(new UserExistsMiddleware($server))
+        ->linkWith(new RoleCheckMiddleware());
+
+    // The server gets a chain from the client code.
+    $server->setMiddleware($middleware);
+
+    do {
+        echo "\nEnter your email:\n";
+        $email = readline();
+        echo "Enter your password:\n";
+        $password = readline();
+        $success  = $server->logIn($email, $password);
+    } while (!$success);
+
+
+    //    (new App())->run();
 } catch (Throwable $e) {
     dd($e->getMessage(), $e->getLine(), $e->getFile(), $e->getTraceAsString());
 }
